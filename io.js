beefac8319a4c597b7eeb60c25c62208aafb533a
Alan Lu <gotoalanlu@gmail.com>
2014-11-02 00:19:32 -0700

    User sessions now handled like a boss

--- io.js
+++ io.js
@@ -1,3 +1,27 @@
+// HACK: YUCK YUCK EW
+// Speed up calls to hasOwnProperty
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+function isEmpty(obj) {
+
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+}
+
 var debug = require('debug')('chesscrow');
 var cookie = require('cookie'),
     cookieParser = require('cookie-parser');
@@ -41,20 +65,34 @@ io.set('authorization', function(data, accept) {
 var activeUsers = {};
 
 io.on('connection', function(socket) {
-    var hs = socket.handshake;
-    // debug('handshake: ' + JSON.stringify(hs));
+    var hs = socket.handshake,
+        cookie = hs.headers.cookie;
+    debug('handshake: ' + JSON.stringify(hs));
 
-    sessionFromCookie(hs.headers.cookie, function(err, s) {
+    sessionFromCookie(cookie, function(err, s) {
         session = new Session(hs, s);
 
         new User({ id: session.passport.user }).fetch().then(function(user) {
             if(user) {
                 debug('user ' + user.id + ' (' + user.get('username') + ') connected');
-                activeUsers[user.id] = user.get('username');
+
+                if(!(user.id in activeUsers)) {
+                    activeUsers[user.id] = { 'username': user.get('username'), 'activeCookies': {} };
+                }
+                activeUsers[user.id].activeCookies[cookie] = Date.now();
+                
                 io.emit('active users update', activeUsers);
 
+                socket.on('ping', function() {
+                    activeUsers[user.id].activeCookies[cookie] = Date.now();
+                });
+
                 socket.on('disconnect', function() {
-                    delete activeUsers[user.id];
+                    delete activeUsers[user.id].activeCookies[cookie];
+                    if(isEmpty(activeUsers[user.id].activeCookies)) {
+                        delete activeUsers[user.id];
+                    }
+
                     io.emit('active users update', activeUsers);
                     debug('user ' + user.id + ' (' + user.get('username') + ') disconnected');
                 });
